7eec561ab485d8126385f3276295fcca872c0b1d
ohad2916 <ohadagadi50@gmail.com>
2023-02-16 17:07:04 +0200

    On branch master  Changes to be committed: 	deleted:    .vs/USC/FileContentIndex/01d66c88-f32a-4f4f-89eb-3d322f7a9777.vsidx	deleted:    .vs/USC/FileContentIndex/39c6152d-c53b-4eeb-9771-2ce2b7e0e516.vsidx 	deleted:    .vs/USC/FileContentIndex/b052cfc1-ef63-4e2b-9d12-036920d7d19b.vsidx 	deleted:    .vs/USC/FileContentIndex/d619bb47-2858-4399-bb04-940cc67a8efc.vsidx 	modified:   .vs/USC/v17/.suo 	modified:   .vs/USC/v17/Browse.VC.db 	modified:   .vs/USC/v17/ipch/AutoPCH/3d9041849cf0a655/MAIN.ipch 	modified:   USC.vcxproj 	modified:   USC.vcxproj.filters 	modified:   main.c 	modified:   spkmeans.h 	modified:   x64/Debug/USC.exe 	modified:   x64/Debug/USC.ilk 	modified:   x64/Debug/USC.log 	modified:   x64/Debug/USC.pdb 	modified:   x64/Debug/USC.tlog/CL.read.1.tlog 	modified:   x64/Debug/USC.tlog/link.read.1.tlog 	modified:   x64/Debug/USC.tlog/link.write.1.tlog 	modified:   x64/Debug/main.obj 	modified:   x64/Debug/vc143.idb 	modified:   x64/Debug/vc143.pdb
     Untracked files:
    	.vs/USC/FileContentIndex/5ae07f82-43cb-408b-91cd-8add3a86be92.vsidx
    	.vs/USC/FileContentIndex/959a40af-a313-46de-9902-d94ed87ada79.vsidx
    	.vs/USC/FileContentIndex/b2427148-505f-4191-8d16-50a056c8e4a8.vsidx
    	.vs/USC/FileContentIndex/ea2a5c7c-a619-4cd2-8316-0df836c54334.vsidx
    	.vs/USC/v17/Browse.VC.db-shm
    	.vs/USC/v17/Browse.VC.db-wal
    	.vs/USC/v17/Browse.VC.opendb
    	example matrix.txt
    	x64/Debug/main.obj.enc

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "spkmeans.h"
 #pragma warning(disable:4996)
 
@@ -45,61 +46,69 @@ int main() {
 		if (!fscanf(file, "%lf\n", &points_list[i][dimension - 1]))
 			return 1;
 	}
-	print_matrix(points_list, point_count, dimension);
+	//print_matrix(points_list, point_count, dimension);
 	/*allocating weighted adjacency matrix*/
-	double** adj_matrix;
-	double* adj_data;
-	if (!(adj_matrix = malloc(sizeof(double*) * point_count))) {
-		printf("eror allocating");
-		return 1;
-	}
-	if (!(adj_data = malloc(sizeof(double) * point_count * point_count))) {
-		printf("eror allocating");
-		return 1;
-	}
-	for (i = 0; i < point_count; i++) {
-		adj_matrix[i] = adj_data + i * point_count;
-	}
+	MATRIX adj_matrix = allocate_square_matrix(point_count);
 	/*calculating the matrix*/
 	weighted_adj_calc(points_list, adj_matrix, point_count, dimension);
-	print_matrix(adj_matrix, point_count, point_count);
+	//print_matrix(adj_matrix, point_count, point_count);
 
 	/*allocating diagonal degree matrix*/
-	double** diag_matrix;
-	double* diag_data;
-	if (!(diag_matrix = malloc(sizeof(double*) * point_count))) {
-		printf("eror allocating");
-		return 1;
-	}
-	if (!(diag_data = calloc(point_count*point_count,sizeof(double)))){
-		printf("eror allocating");
-		return 1;
-	}
-	for (i = 0; i < point_count; i++) {
-		diag_matrix[i] = diag_data + i * point_count;
-	}
+	MATRIX diag_matrix = allocate_square_matrix(point_count);
 	/*calculating diagonal matrix*/
 	diagonal_deg_calc(adj_matrix, diag_matrix, point_count);
-	print_matrix(diag_matrix, point_count, point_count);
+	//print_matrix(diag_matrix, point_count, point_count);
 
 	/*allocating graph laplacian matrix*/
-	double** laplac_matrix;
-	double* laplac_data;
-	if (!(laplac_matrix = malloc(sizeof(double*) * point_count))) {
-		printf("eror allocating");
-		return 1;
-	}
-	if (!(laplac_data = calloc(point_count * point_count, sizeof(double)))) {
-		printf("eror allocating");
-		return 1;
-	}
-	for (i = 0; i < point_count; i++) {
-		laplac_matrix[i] = laplac_data + i * point_count;
-	}
+	MATRIX laplac_matrix = allocate_square_matrix(point_count);
 	/*calculating graph laplacian*/
 	graph_laplacian_calc(adj_matrix, diag_matrix, laplac_matrix, point_count);
 	print_matrix(laplac_matrix, point_count, point_count);
 
+	/*debuging pivot*/
+	MATRIX pivot_matrix = generate_pivot_matrix(laplac_matrix, point_count);
+	print_matrix(pivot_matrix, point_count, point_count);
+	printf("\n\n");
+	/*jacobis*/
+	MATRIX _A = allocate_square_matrix(3);
+	_A[0][0] = 1;_A[0][1] = 0;_A[0][2] = 0;
+	_A[1][0] = 0;_A[1][1] = 2;_A[1][2] = -1;
+	_A[2][0] = 0;_A[2][1] = -1;_A[2][2] = 2;
+	MATRIX_LIST p_list;
+	p_list.this = NULL;
+	p_list.next = NULL;
+
+	for (i = 0; i < 340; i++) {
+		MATRIX pivot_matrix = generate_pivot_matrix(_A, 3);
+		append(pivot_matrix, &p_list);
+		//print_matrix(pivot_matrix, 3, 3);
+		MATRIX dot_res = matrix_dot(_A, pivot_matrix, 3);
+		MATRIX transposed_pivot = transpose_matrix(pivot_matrix, 3);
+		MATRIX second_dot_res = matrix_dot(transposed_pivot, dot_res,3);
+		free(*transposed_pivot);
+		free(*dot_res);
+		free(*_A);
+		_A = second_dot_res;
+		print_matrix(_A, 3, 3);
+		print_matrix(pivot_matrix, 3, 3);
+		char c;
+		scanf("%c",&c);
+	}
+	print_matrix(_A,3, 3);
+	MATRIX dot_res = allocate_square_matrix(3);
+	for (i = 0; i < 3; i++) {
+		dot_res[i][i] = 1;
+	}
+	MATRIX temp_res = NULL;;
+	do {
+		temp_res = matrix_dot(dot_res, p_list.this,3);
+		free(dot_res);
+		dot_res = temp_res;
+		//free(*p_list.this);
+		p_list = *p_list.next;
+		
+	} while (p_list.next != NULL);
+	print_matrix(dot_res, 3, 3);
 
 	return 1;
 }
--- spkmeans.h
+++ spkmeans.h
@@ -1,5 +1,7 @@
 #include <math.h>
 #include <stdio.h>
+#define MATRIX double**
+#define VECTOR double*
 
 double two_norm(double* a, double* b, size_t dimension) {
 	size_t i = 0;
@@ -9,6 +11,118 @@ double two_norm(double* a, double* b, size_t dimension) {
 	}
 	return sum;
 }
+
+typedef struct m_list{
+	MATRIX this;
+	struct m_list* next;
+}MATRIX_LIST;
+
+int append(MATRIX a, MATRIX_LIST* list) {
+	if (!list->this)
+		list->this = a;
+	else {
+		while (list->next)
+			list = list->next;
+		MATRIX_LIST* new = malloc(sizeof(MATRIX_LIST));
+		new->next = NULL;
+		new->this = a;
+		list->next = new;
+	}
+
+}
+
+MATRIX allocate_square_matrix(size_t n) {
+	double** matrix;
+	double* data;
+	size_t i;
+	if (!(matrix = malloc(sizeof(double*) * n))) {
+		printf("error allocating");
+		return 1;
+	}
+	if (!(data = calloc(n*n, sizeof(double)))) {
+		printf("error allocating");
+		return 1;
+	}
+	for (i = 0; i <n; i++) {
+		matrix[i] = data + i * n;
+	}
+	return matrix;
+}
+
+MATRIX generate_pivot_matrix(MATRIX _A,size_t n) {
+	MATRIX _P = allocate_square_matrix(n);
+	/*finding arg(s)max A(i,j)*/
+	size_t i, j;
+	size_t argmax_i = 0, argmax_j = 0;
+	double max_value = -1.0;
+	double phi,c,t,s;
+	int sign = 0;
+	for (i = 0; i < n; i++) {
+		for (j = i + 1; j < n; j++) {
+			if (fabs(_A[i][j]) > max_value) {
+				argmax_i = i;
+				argmax_j = j;
+				max_value = fabs(_A[i][j]);
+			}
+		}
+	}
+	/*obtaining c,t*/
+	phi = (_A[argmax_j][argmax_j] - _A[argmax_i][argmax_i]) / (2.0 * _A[argmax_i][argmax_j]);
+	sign += (phi >= 0);
+	sign -= (sign == 0);
+	t = sign / ((abs(phi) + sqrt(phi * phi + 1)));
+	c = 1 / (sqrt(t * t + 1));
+	s = t * c;
+	/*ammending pivot matrix*/
+	for (i = 0; i < n; i++) {
+		_P[i][i] = 1;
+	}
+	_P[argmax_i][argmax_i] = c;
+	_P[argmax_i][argmax_j] = s;
+	_P[argmax_j][argmax_j] = c;
+	if(s != 0.0)
+		_P[argmax_j][argmax_i] = -s;
+	
+	return _P;
+}
+
+double vector_dot(VECTOR x, VECTOR y, size_t n) {
+	size_t i;
+	double sum = 0;
+	for (i = 0; i < n; i++) {
+		sum += x[i] * y[i];
+	}
+	return sum;
+}
+
+MATRIX matrix_dot(MATRIX a, MATRIX b, size_t n) {
+	size_t i, j,k;
+	MATRIX dot = allocate_square_matrix(n);
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			for (k = 0;k < n;k++) {
+				dot[i][j] += a[i][k] * b[k][j];
+			}
+		}
+	}
+	return dot;
+}
+
+MATRIX transpose_matrix(MATRIX p,size_t n) {
+	size_t i, j;
+	double temp;
+	MATRIX res = allocate_square_matrix(n);
+	for (i = 0; i < n; i++) {
+		for (j = i+1; j < n; j++) {
+			res[j][i] = p[i][j];
+			res[i][j] = p[j][i];
+		}
+		res[i][i] = p[i][i];
+	}
+	return res;
+}
+
+
 int weighted_adj_calc(double** points, double** target, size_t no_points,size_t dimension) {
 	size_t i;
 	size_t j;
