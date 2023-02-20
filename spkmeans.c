cd2707b3c56006f7a83e7a9d8a9eb749f0f01634
ohad2916 <ohadagadi50@gmail.com>
2023-02-20 10:37:54 +0200

    fixed -ansi -pedantic compiling warnnings fixed convergence c-part probably done

--- spkmeans.c
+++ spkmeans.c
@@ -1,15 +1,31 @@
 #include <string.h>
 #include "spkmeans.h"
-#pragma warning(disable:4996)
+/*#pragma warning(disable:4996)*/
+
 
 int main(int argc, char* argv[]) {
-	//handeling cmdline arguments
-	FILE* file = fopen(argv[2], "r");
+	char char_surfer = 'f';
+	size_t commas = 0;
+	size_t dimension = 1;
+	size_t point_count = 0;
+	size_t j,i;
+	int return_value = 0;
+	int mode;
+	double** points_list;
+	double* p;
+	EIGEN_VALUES_VECTORS* jacobis_res;
+	FILE* file;
+	MATRIX weight_adj;MATRIX diag_degree_mat;MATRIX graph_laplacian;
+	/*handeling cmdline arguments*/
+	if (argc != 3) {
+		printf("An Error Has Occured");
+		return 1;
+	}
+	file = fopen(argv[2], "r");
 	if (!file) {
 		printf("an Error has occured!");
 		return 1;
 	}
-	int mode;
 	if (strcmp(argv[1], "wam") == 0)
 		mode = 0;
 	else if (strcmp(argv[1], "ddg") == 0)
@@ -23,12 +39,6 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 	/*reading the file*/
-	char char_surfer = 'f';
-	size_t commas = 0;
-	size_t dimension = 1;
-	size_t point_count = 0;
-	size_t j;
-	size_t i;
 	while ((char_surfer = fgetc(file)) != '\n' && char_surfer != EOF) {
 		dimension += (char_surfer == ',');
 	}
@@ -37,17 +47,15 @@ int main(int argc, char* argv[]) {
 		commas += (char_surfer == ',');
 	}
 	point_count = commas / (dimension - 1);
-	printf("dimension is: %d\nno.points:%d\n", dimension, point_count);
+	/*printf("dimension is: %lu\nno.points:%lu\n", dimension, point_count);*/
 	rewind(file);
 
-	double** points_list;
 	if (!(points_list = malloc(sizeof(double*) * point_count))) {
-		printf("error allocating");
+		printf("an Error has occured!");
 		return 1;
 	}
-	double* p;
 	if (!(p = malloc(sizeof(double) * point_count * dimension))) {
-		printf("error allocating");
+		printf("an Error has occured!");
 		return 1;
 	}
 	for (i = 0; i < point_count; i++) {
@@ -56,70 +64,79 @@ int main(int argc, char* argv[]) {
 
 	for (i = 0; i < point_count; i++) {
 		for (j = 0; j < dimension - 1; j++) {
-			if (!fscanf(file, "%lf,", &points_list[i][j]))
+			if (!fscanf(file, "%lf,", &points_list[i][j])) {
+				freeMatrix(points_list);
 				return 1;
+			}
 		}
-		if (!fscanf(file, "%lf\n", &points_list[i][dimension - 1]))
+		if (!fscanf(file, "%lf\n", &points_list[i][dimension - 1])) {
+			freeMatrix(points_list);
 			return 1;
+		}
 	}
+	fclose(file);
 	/*end reading file*/
-	MATRIX weight_adj = weightedAdjacencyMatrix(points_list, point_count, dimension);
+	weight_adj = weightedAdjacencyMatrix(points_list, point_count, dimension);
 	if (mode == 0) {
-		printf("GOAL 0\n");
-		printMatrix(weight_adj, point_count, point_count);
-		free(*weight_adj);
-		free(weight_adj);
-		free(*points_list);
-		free(points_list);
-		return 0;
+		if (weight_adj)
+			printMatrix(weight_adj, point_count, point_count);
+		else {
+			return_value = 1;
+			printf("an Error has occured!");
+		}
+		freeMatrix(weight_adj);
+		freeMatrix(points_list);
+		return return_value;
 	}
-	MATRIX diag_degree_mat = diagonalDegreeMatrix(weight_adj, point_count);
+	diag_degree_mat = diagonalDegreeMatrix(weight_adj, point_count);
 	if (mode == 1) {
-		printf("GOAL 1\n");
-		printMatrix(diag_degree_mat, point_count, point_count);
-		free(*diag_degree_mat);
-		free(diag_degree_mat);
-		free(*weight_adj);
-		free(weight_adj);
-		free(*points_list);
-		free(points_list);
-		return 0;
+		if (diag_degree_mat)
+			printMatrix(diag_degree_mat, point_count, point_count);
+		else {
+			return_value = 1;
+			printf("an Error has occured!");
+		}
+		freeMatrix(diag_degree_mat);
+		freeMatrix(weight_adj);
+		freeMatrix(points_list);
+		return return_value;
 	}
-	MATRIX graph_laplacian = graphLaplacian(weight_adj, diag_degree_mat, point_count);
+	graph_laplacian = graphLaplacian(weight_adj, diag_degree_mat, point_count);
 	if (mode == 2) {
 		printf("GOAL 2\n");
-		printMatrix(graph_laplacian,point_count,point_count);
-		free(*graph_laplacian);
-		free(graph_laplacian);
-		free(*diag_degree_mat);
-		free(diag_degree_mat);
-		free(*weight_adj);
-		free(weight_adj);
-		free(*points_list);
-		free(points_list);
-		return 0;
+		if (graph_laplacian)
+			printMatrix(graph_laplacian, point_count, point_count);
+		else {
+			return_value = 1;
+			printf("an Error has occured!");
+		}
+		freeMatrix(graph_laplacian);
+		freeMatrix(diag_degree_mat);
+		freeMatrix(weight_adj);
+		freeMatrix(points_list);
+		return return_value;
 	}
-	printf("GOAL 3\n");
-	EIGEN_VALUES_VECTORS* jacobis_res = jacobis(graph_laplacian, point_count, 100, 0.000001);
-	printVector(jacobis_res->eigen_values,point_count);
-	printMatrix(jacobis_res->eigen_vectors_as_columns, point_count, point_count);
+	jacobis_res = jacobis(graph_laplacian, point_count, 100, 0.000001);
+	if (jacobis_res) {
+		printVector(jacobis_res->eigen_values, point_count);
+		printMatrix(jacobis_res->eigen_vectors_as_columns, point_count, point_count);
+	}
+	else {
+		return_value = 1;
+		printf("an Error has occured!");
+	}
 
 	/*cleanup*/
-	free(*graph_laplacian);
-	free(graph_laplacian);
-	free(*diag_degree_mat);
-	free(diag_degree_mat);
-	free(*weight_adj);
-	free(weight_adj);
-	free(*points_list);
-	free(points_list);
+	freeMatrix(graph_laplacian);
+	freeMatrix(diag_degree_mat);
+	freeMatrix(weight_adj);
+	freeMatrix(points_list);
 	free(jacobis_res->eigen_values);
-	free(*(jacobis_res->eigen_vectors));
-	free(jacobis_res->eigen_vectors);
-	free(*(jacobis_res->eigen_vectors_as_columns));
-	free(jacobis_res->eigen_vectors_as_columns);
+	freeMatrix(jacobis_res->eigen_vectors);
+	freeMatrix(jacobis_res->eigen_vectors_as_columns);
 	free(jacobis_res);
-	return 0;
+	
+	return return_value;
 
 
 
--- spkmeans.h
+++ spkmeans.h
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #define MATRIX double**
 #define VECTOR double*
 
@@ -13,10 +14,6 @@ double twoNorm(double* a, double* b, size_t dimension) {
 	return sum;
 }
 
-typedef struct m_list{
-	MATRIX this;
-	struct m_list* next;
-}MATRIX_LIST;
 typedef struct eigen_v {
 	MATRIX eigen_vectors;
 	MATRIX eigen_vectors_as_columns;
@@ -27,19 +24,12 @@ typedef struct k_eigenvs {
 	size_t k;
 }FIRST_K_EIGEN;
 
-int append(MATRIX a, MATRIX_LIST* list) {
-	if (!list->this)
-		list->this = a;
-	else {
-		while (list->next)
-			list = list->next;
-		MATRIX_LIST* new = malloc(sizeof(MATRIX_LIST));
-		new->next = NULL;
-		new->this = a;
-		list->next = new;
-	}
-
+int freeMatrix(MATRIX a) {
+	free(*a);
+	free(a);
+	return 0;
 }
+
 
 MATRIX allocateSquareMatrix(size_t n) {
 	double** matrix;
@@ -59,15 +49,16 @@ MATRIX allocateSquareMatrix(size_t n) {
 }
 
 MATRIX generatePivotMatrix(MATRIX _A,size_t n) {
+	size_t i, j,argmax_i,argmax_j;
+	double max_value = -1.0;
+	double phi,c,t,s;
+	int sign;
 	MATRIX _P = allocateSquareMatrix(n);
 	if (!_P)
 		return NULL;
 	/*finding arg(s)max A(i,j)*/
-	size_t i, j;
-	size_t argmax_i = 0, argmax_j = 0;
-	double max_value = -1.0;
-	double phi,c,t,s;
-	int sign = 0;
+	argmax_i = 0; argmax_j = 0;
+	sign = 0;
 	for (i = 0; i < n; i++) {
 		for (j = i + 1; j < n; j++) {
 			if (fabs(_A[i][j]) > max_value) {
@@ -81,7 +72,7 @@ MATRIX generatePivotMatrix(MATRIX _A,size_t n) {
 	phi = (_A[argmax_j][argmax_j] - _A[argmax_i][argmax_i]) / (2.0 * _A[argmax_i][argmax_j]);
 	sign += (phi >= 0);
 	sign -= (sign == 0);
-	t = sign / ((abs(phi) + sqrt(phi * phi + 1)));
+	t = sign / ((fabs(phi) + sqrt(phi * phi + 1)));
 	c = 1 / (sqrt(t * t + 1));
 	s = t * c;
 	/*ammending pivot matrix*/
@@ -126,7 +117,7 @@ MATRIX transposeMatrix(MATRIX p,size_t n,const char* how){
 	double temp1;
 	double temp2;
 	MATRIX res;
-	if (how != "in-place") {
+	if (strcmp(how,"in-place") != 0) {
 		res = allocateSquareMatrix(n);
 		if (!res)
 			return NULL;
@@ -148,15 +139,17 @@ MATRIX transposeMatrix(MATRIX p,size_t n,const char* how){
 }
 
 EIGEN_VALUES_VECTORS* jacobis(MATRIX _A,size_t n,size_t iteration_limit, double epsilon) {
+	double current_off_diag_sum = 2147483647;
+	double new_off_diag_sum = .0;
+	MATRIX dotted_pivots = NULL; MATRIX pivot_matrix = NULL;MATRIX temp_pivot_pointer = NULL;
+	MATRIX dot_res = NULL;MATRIX transposed_pivot = NULL;MATRIX second_dot_res = NULL;
+	VECTOR eigen_values = NULL;
+	size_t i, j,k;
 	EIGEN_VALUES_VECTORS* res = malloc(sizeof(EIGEN_VALUES_VECTORS));
 	if (!res) {
 		return NULL;
 	}
-	double current_off_diag_sum = -1.0;
-	double new_off_diag_sum = .0;
-	double convergence_diff = .0;
-	size_t i, j,k;
-	MATRIX dotted_pivots = allocateSquareMatrix(n);
+	dotted_pivots = allocateSquareMatrix(n);
 	if (!dotted_pivots) {
 		free(res);
 		return NULL;
@@ -166,14 +159,14 @@ EIGEN_VALUES_VECTORS* jacobis(MATRIX _A,size_t n,size_t iteration_limit, double 
 	}
 
 	for (i = 0; i < iteration_limit; i++) {
-		MATRIX pivot_matrix = generatePivotMatrix(_A, n);
+		pivot_matrix = generatePivotMatrix(_A, n);
 		if (!pivot_matrix) {
 			free(res);
 			freeMatrix(dotted_pivots);
 			return NULL;
 		}
 		/*generating eigen vector matrix */
-		MATRIX temp_pivot_pointer = matrixDot(dotted_pivots, pivot_matrix,n);
+		temp_pivot_pointer = matrixDot(dotted_pivots, pivot_matrix,n);
 		if (!temp_pivot_pointer) {
 			free(res);
 			freeMatrix(pivot_matrix);
@@ -182,15 +175,15 @@ EIGEN_VALUES_VECTORS* jacobis(MATRIX _A,size_t n,size_t iteration_limit, double 
 		}
 		freeMatrix(dotted_pivots);
 		dotted_pivots = temp_pivot_pointer;
-		MATRIX dot_res = matrixDot(_A, pivot_matrix, n);
+		dot_res = matrixDot(_A, pivot_matrix, n);
 		if (!dot_res) {
 			free(res);
 			freeMatrix(pivot_matrix);
 			freeMatrix(temp_pivot_pointer);
 			return NULL;
 		}
-		MATRIX transposed_pivot = transposeMatrix(pivot_matrix, n,"in-place");
-		MATRIX second_dot_res = matrixDot(transposed_pivot, dot_res, n);
+		transposed_pivot = transposeMatrix(pivot_matrix, n,"in-place");
+		second_dot_res = matrixDot(transposed_pivot, dot_res, n);
 		if (!second_dot_res) {
 			free(res);
 			freeMatrix(pivot_matrix);
@@ -202,21 +195,25 @@ EIGEN_VALUES_VECTORS* jacobis(MATRIX _A,size_t n,size_t iteration_limit, double 
 			free(_A);
 		}
 		_A = second_dot_res;
-		//checking convergence
+		/*checking convergence*/
 		new_off_diag_sum = 0.0;
 		for (k = 0; k < n; k++) {
-			for (j = n+1; j < n; j++) {
-				new_off_diag_sum += _A[i][j]* _A[i][j];
+			for (j = k+1; j < n; j++) {
+				new_off_diag_sum += _A[k][j] * _A[k][j];
 			}
 		}
-		new_off_diag_sum *= 2;
-		/*if (current_off_diag_sum - new_off_diag_sum <= epsilon)
-			break;*/
-		//cleanup
+		new_off_diag_sum *= 2.0;
+		/*cleanup*/
 		free(*transposed_pivot);
 		free(transposed_pivot);
 		free(*dot_res);
 		free(dot_res);
+
+		/*breaking in case converged*/
+		if ((current_off_diag_sum - new_off_diag_sum) <= epsilon) {
+			break;
+		}
+		current_off_diag_sum = new_off_diag_sum;
 	}
 	
 	res->eigen_vectors_as_columns = dotted_pivots;
@@ -225,7 +222,7 @@ EIGEN_VALUES_VECTORS* jacobis(MATRIX _A,size_t n,size_t iteration_limit, double 
 		freeMatrix(dotted_pivots);
 		free(res);
 	}
-	VECTOR eigen_values = malloc(sizeof(double) * n);
+	eigen_values = malloc(sizeof(double) * n);
 	if (!eigen_values) {
 		freeMatrix(dotted_pivots);
 		freeMatrix(res->eigen_vectors);
@@ -242,14 +239,14 @@ EIGEN_VALUES_VECTORS* jacobis(MATRIX _A,size_t n,size_t iteration_limit, double 
 size_t find_k(VECTOR e_values,size_t n) {
 	size_t i;
 	double max_gap = .0;
-	size_t argMax = 0;
+	size_t argMax;
 	for ( i = 0; i < n - 1; i++) {
 		if (fabs(e_values[i + 1] - e_values[i]) > max_gap) {
 			max_gap = fabs(e_values[i + 1] - e_values[i]);
 			argMax = i;
 		}
 	}
-	return i;
+	return argMax;
 }
 
 int sortEigenVectors(EIGEN_VALUES_VECTORS* subject,size_t n) {
@@ -277,12 +274,12 @@ int sortEigenVectors(EIGEN_VALUES_VECTORS* subject,size_t n) {
 }
 
 MATRIX weightedAdjacencyMatrix(double** points, size_t n,size_t dimension) {
+	size_t i;
+	size_t j;
 	MATRIX res = allocateSquareMatrix(n);
 	if (!res) {
 		return NULL;
 	}
-	size_t i;
-	size_t j;
 	for (i = 0; i < n; i++) {
 		for (j = i; j < n;j++) {
 			res[i][j] = twoNorm(points[i], points[j], dimension);
@@ -293,11 +290,11 @@ MATRIX weightedAdjacencyMatrix(double** points, size_t n,size_t dimension) {
 }
 
 MATRIX diagonalDegreeMatrix(double** adj_matrix, size_t n) {
+	size_t i;
+	size_t j;
 	MATRIX res = allocateSquareMatrix(n);
 	if (!res)
 		return NULL;
-	size_t i;
-	size_t j;
 	for (i = 0; i < n; i++) {
 		for (j = 0; j < n; j++) {
 			res[i][i] += adj_matrix[i][j];
@@ -307,10 +304,10 @@ MATRIX diagonalDegreeMatrix(double** adj_matrix, size_t n) {
 }
 
 MATRIX graphLaplacian(double** adj_matrix, double** diag_matrix, size_t n) {
+	size_t i, j;
 	MATRIX res = allocateSquareMatrix(n);
 	if (!res)
 		return NULL;
-	size_t i, j;
 	for (i = 0; i < n; i++) {
 		for (j = i; j < n; j++) {
 			res[i][j] = diag_matrix[i][j] - adj_matrix[i][j];
@@ -321,7 +318,7 @@ MATRIX graphLaplacian(double** adj_matrix, double** diag_matrix, size_t n) {
 }
 
 int printMatrix(double** mat, size_t rows, size_t cols) {
-	int i = 0; int j = 0;
+	size_t i = 0; size_t j = 0;
 	for (i = 0; i < rows; i++) {
 		for (j = 0; j < cols - 1; j++) {
 			printf("%.2f,", mat[i][j]);
@@ -340,8 +337,3 @@ int printVector(VECTOR v, size_t n) {
 	return 0;
 }
 
-int freeMatrix(MATRIX a) {
-	free(*a);
-	free(a);
-	return 0;
-}
